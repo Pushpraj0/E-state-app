6d22122547e752e9dd6461e733b84760df45e2d8
Pushpraj0 <97541059+Pushpraj0@users.noreply.github.com>
2025-05-26 19:53:29 +0530

    changes

--- Client/src/components/map/Map.jsx
+++ Client/src/components/map/Map.jsx
@@ -3,6 +3,19 @@ import './map.scss';
 import 'leaflet/dist/leaflet.css'
 import Pin from '../pin/Pin';
 
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 export default function Map({items}) {
   return (
     <MapContainer center={items.length === 1 ? [items[0].latitude, items[0].longitude] : [51.5074,-0.1278]} zoom={7} scrollWheelZoom={true} className='map'>
